9dbcc7b4690d064b1b6ae96c94cd3ea191134c92
Jessica Edouard <jedouard@jedouard-macbookpro.roam.corp.google.com>
2018-07-13 12:55:08 -0700

    Added loading image setting script

--- GooglePlayInstant/Editor/PlayInstantLoadingScreenGenerator.cs
+++ GooglePlayInstant/Editor/PlayInstantLoadingScreenGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEditor;
 using UnityEngine;
 using UnityEditor.SceneManagement;
+using UnityEngine.UI;
 using System;
 using System.IO;
 
@@ -10,20 +11,33 @@ namespace GooglePlayInstant.Editor
     {
         public static void GenerateLoadingScreenScene(string pathToLoadingScreenImage, string assetBundleUrl)
         {
-            GenerateLoadingScreenScript(assetBundleUrl);
-            //create the scene and the game object
             var scene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Additive);
             var loadingScreenGO = new GameObject("Canvas");
             
+            GenerateLoadingScreenScript(assetBundleUrl);
+            AddBackgroundImageToScene(loadingScreenGO, pathToLoadingScreenImage);
+        }
+        
+        private static void AddBackgroundImageToScene(GameObject loadingScreenGO, string pathToLoadingScreenImage)
+        {
             loadingScreenGO.AddComponent<Canvas>();
-            
             var loadingScreenCanvas = loadingScreenGO.GetComponent<Canvas>();
             loadingScreenCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
-   
+            
+            byte[] loadingScreenImageData = File.ReadAllBytes(pathToLoadingScreenImage);
+            var tex = new Texture2D(1, 1);
+            tex.LoadImage(loadingScreenImageData);
+
+            var loadingImageSprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+
+            loadingScreenGO.AddComponent<Image>();
+            var loadingScreenImage = loadingScreenGO.GetComponent<Image>();
+            loadingScreenImage.sprite = loadingImageSprite;
         }
 
         private static void GenerateLoadingScreenScript(string assetBundleUrl)
         {
+            //TODO: add better handling of finding assets folder
             var newloadingScreenScriptDir = Directory.GetCurrentDirectory() + "/Assets/GooglePlayInstantScript/LoadingScreenScript.cs";
             if (Directory.GetFiles(Directory.GetCurrentDirectory(), "GenericLoadingScreenScript.cs",
                     SearchOption.AllDirectories).Length == 0)
