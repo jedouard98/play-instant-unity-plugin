aac643918b7f99ca2d221b4c31f69606dc523047
Audace Nakeshimana <audace@google.com>
2018-07-31 14:49:58 -0700

    Implementing functionality

--- GooglePlayInstant/Editor/PlayInstantQuickDeployGCPClient.cs
+++ GooglePlayInstant/Editor/PlayInstantQuickDeployGCPClient.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
 using System.Reflection.Emit;
 using System.Security.Cryptography;
 using System.Threading;
+using Boo.Lang;
 using GooglePlayInstant.Editor.GooglePlayServices;
 using NUnit.Framework;
 using UnityEditor;
@@ -19,6 +21,9 @@ namespace GooglePlayInstant.Editor
     /// </summary>
     /// <param name="www"></param>
     public delegate void HttpResponseCallback(WWW www);
+
+    public delegate void AuthorizationResponseCallback(KeyValuePair<string, string> response);
+
     public static class QuickDeployTokenUtility
     {
         private const string GrantType = "authorization_code";
@@ -26,7 +31,12 @@ namespace GooglePlayInstant.Editor
         private static AccessToken _accessToken;
 
         public delegate void AuthCodeReceivedCallback(AuthorizationCode authorizationCode);
+
         public delegate void TokenReceivedCallback(AccessToken accessToken);
+
+        public static AuthorizationResponseCallback oAuthCodeReceivedCallback;
+
+        public static KeyValuePair<string, string>? AuthResponse;
 
         public static AccessToken AccessToken
         {
@@ -34,38 +44,52 @@ namespace GooglePlayInstant.Editor
         }
 
 
+        public static void OnGUI()
+        {
+            // Handle Authorization code tasks
+            if (oAuthCodeReceivedCallback != null & AuthResponse.HasValue)
+            {
+                oAuthCodeReceivedCallback.Invoke(AuthResponse.Value);
+                oAuthCodeReceivedCallback = null;
+            }
+            
+            // Display statuses for the requests in progress
+            WwwRequestInProgress.UpdateState();
+            WwwRequestInProgress.DisplayProgressForTrackedRequests();
+        }
+
         public static void ScheduleAuthCode(AuthCodeReceivedCallback authCodeReceivedCallback)
         {
             QuickDeployOAuth2Server server = new QuickDeployOAuth2Server();
             server.Start();
             var redirect_uri = server.CallbackEndpoint;
+
+
+            AuthorizationResponseCallback responseCallback = responsePair =>
+            {
+                if (!string.Equals("code", responsePair.Key))
+                {
+                    throw new InvalidStateException("Could not receive needed permissions");
+                }
+
+                AuthorizationCode authCode = new AuthorizationCode
+                {
+                    code = responsePair.Value,
+                    redirect_uri = redirect_uri
+                };
+                if (authCodeReceivedCallback != null)
+                {
+                    authCodeReceivedCallback.Invoke(authCode);
+                }
+            };
+            oAuthCodeReceivedCallback = responseCallback;
+            // Now ask permissions from the server.
             Oauth2Credentials credentials = ReadOauth2CredentialsFile();
             string queryParams = "?scope=" + Scope + "&access_type=offline&include_granted_scopes=true" +
                                  "&redirect_uri=" + redirect_uri + "&response_type=code" + "&client_id=" +
                                  credentials.client_id;
             var authorizatonUrl = credentials.auth_uri + queryParams;
-            Process.Start(authorizatonUrl);
-            while (!server.HasOauth2AuthorizationResponse())
-            {
-                Debug.Log("No response yet");
-                Thread.Sleep(1);
-            }
-
-            KeyValuePair<string, string> response = server.GetAuthorizationResponse();
-            if (!string.Equals("code", response.Key))
-            {
-                throw new InvalidStateException("Could not receive needed permissions");
-            }
-
-            AuthorizationCode authCode = new AuthorizationCode
-            {
-                code = response.Value,
-                redirect_uri = redirect_uri
-            };
-            if (authCodeReceivedCallback != null)
-            {
-                authCodeReceivedCallback.Invoke(authCode);
-            }
+            Application.OpenURL(authorizatonUrl);
         }
 
         public static void ScheduleAccessToken(AuthorizationCode authCode, TokenReceivedCallback tokenReceivedCallback)
@@ -84,8 +108,9 @@ namespace GooglePlayInstant.Editor
             formData.Add("redirect_uri", authCode.redirect_uri);
             formData.Add("grant_type", GrantType);
 
-            WWW www = QuickDeployWwwRequestHandler.SendHttpPostRequest(tokenEndpiont, formData, headers);
-            WwwRequestInProgress requestInProgress = new WwwRequestInProgress(www, "Downloading access token",
+            WwwRequestInProgress requestInProgress = new WwwRequestInProgress(
+                QuickDeployHttpRequestHelper.SendHttpPostRequest(tokenEndpiont, formData, headers),
+                "Downloading access token",
                 "Getting access token to use for uploading asset bundle");
             requestInProgress.TrackProgress();
             requestInProgress.ScheduleTaskOnDone(doneWww =>
@@ -101,15 +126,11 @@ namespace GooglePlayInstant.Editor
                 tokenReceivedCallback.Invoke(token);
             });
         }
+
         public static Oauth2Credentials ReadOauth2CredentialsFile()
         {
             return JsonUtility.FromJson<Oauth2File>(
                 File.ReadAllText(QuickDeployConfig.Config.cloudCredentialsFileName)).installed;
-        }
-
-        public static void AssignToken(AccessToken accessToken)
-        {
-            _accessToken = accessToken;
         }
     }
 
@@ -127,19 +148,21 @@ namespace GooglePlayInstant.Editor
         /// <summary>
         /// Upload bundle to the cloud, assuming TokenUtility has a valid access token.
         /// </summary>
-        public static void UploadBundle()
+        public static void CreateBucketIfNotExistsAndUploadBundle()
         {
             // TODO(audace): Split this into two tasks, one to carry out when the bucket exists, and one to carry out when the bucket happens to not exist
-            if (!IfBucketExists(_config.cloudStorageBucketName, null, null))
-            {
-                CreateBucket(_config.cloudStorageBucketName, doneWww => { UploadBundle();});
-            }
+            IfBucketExists(_config.cloudStorageBucketName, doneWWW => { UploadBundle(); },
+                doneWWW =>
+                {
+                    ScheduleCreateBucket(_config.cloudStorageBucketName,
+                        bucketCreationResponse => { UploadBundle(); });
+                });
 
-            if (AlwaysTrue())
-            {
-                return;
-            }
+           
+        }
 
+        private static void UploadBundle()
+        {
             var uploadEndpoint =
                 string.Format("https://www.googleapis.com/upload/storage/v1/b/{0}/o?uploadType=media&name={1}",
                     _config.cloudStorageBucketName, _config.cloudStorageFileName);
@@ -149,8 +172,9 @@ namespace GooglePlayInstant.Editor
             var headers = new Dictionary<string, string>();
             headers.Add("Authorization", "Bearer " + AccessToken.access_token);
             headers.Add("Content-Length", bytes.Length.ToString());
-            var result = QuickDeployWwwRequestHandler.SendHttpPostRequest(uploadEndpoint, bytes, headers);
-            
+            var result = QuickDeployHttpRequestHelper.SendHttpPostRequest(uploadEndpoint, bytes, headers);
+            var requestInProgress = new WwwRequestInProgress(result, "Uploading bundle to cloud", "Please wait while your bundle is being uploaded to the cloud");
+            requestInProgress.TrackProgress();
         }
 
         private static bool AlwaysTrue()
@@ -167,35 +191,41 @@ namespace GooglePlayInstant.Editor
                 credentials.project_id);
             Dictionary<string, string> form = new Dictionary<string, string>();
             form.Add("name", bucketName);
-            WWW request = QuickDeployWwwRequestHandler.SendHttpPostRequest(createBucketEndPoint, form, null);
+            WWW request = QuickDeployHttpRequestHelper.SendHttpPostRequest(createBucketEndPoint, form, null);
             WwwRequestInProgress requestInProgress = new WwwRequestInProgress(request,
                 string.Format("Creating bucket with name {0}", bucketName),
                 "You have specified a bucket that does not exist yet. Please wait while it is being created");
             requestInProgress.TrackProgress();
-            requestInProgress.ScheduleTaksOnDone(wwwResult =>
+            requestInProgress.ScheduleTaskOnDone(wwwResult =>
             {
                 if (resultHandler != null)
                 {
                     resultHandler.Invoke(wwwResult);
                 }
-
-                
             });
         }
 
         // Checks whether the bucket with the name bucketName exists. Assumes access token valid.
         // TODO(audace): Implement different paths to be executed on true and on false
-        private static bool IfBucketExists(string bucketName, WwwHandler onTrue, WwwHandler onFalse)
+        private static void IfBucketExists(string bucketName, WwwHandler onTrue, WwwHandler onFalse)
         {
             string bucketInfoUrl =
                 string.Format("https://www.googleapis.com/storage/v1/b/{0}?fields=location", bucketName);
             Dictionary<string, string> headers = new Dictionary<string, string>();
             headers.Add("Authorization", string.Format("Bearer {0}", AccessToken.access_token));
-            var result = QuickDeployWwwRequestHandler.SendHttpGetRequest(bucketInfoUrl, null, headers);
+            var result = QuickDeployHttpRequestHelper.SendHttpGetRequest(bucketInfoUrl, null, headers);
             Debug.LogFormat("Bucket exists result is {0} ", result);
-            return false;
+            if (onTrue != null)
+            {
+                onTrue.Invoke(result);
+            }
+
+            if (onFalse != null)
+            {
+                onFalse.Invoke(result);
+            }
         }
-        
+
         public static void ScheduleUpload(HttpResponseCallback onResponse)
         {
             throw new NotImplementedException("Not Implemented");
